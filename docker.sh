a6d9012de77ed4d948d09b5399ba5e30ce6021a6
anabneri <$git_config_user_email>
2020-04-29 15:20:18 -0300

    :whale: script in en

--- docker.sh
+++ docker.sh
@@ -4,10 +4,10 @@
 
 sudo apt update
 
-echo setando a versão do wsl
+echo updating the version 2 of wsl
 wsl --set-version Ubuntu 2
 
-echo transportando as imagens do docker pro Ubuntu
+echo carrying images from the docker pro Ubuntu
 wget "https://download.docker.com/linux/ubuntu/dists/disco/pool/stable/amd64/containerd.io_1.2.6-3_amd64.deb"
 wget "https://download.docker.com/linux/ubuntu/dists/disco/pool/stable/amd64/docker-ce-cli_19.03.3~3-0~ubuntu-disco_amd64.deb"
 wget "https://download.docker.com/linux/ubuntu/dists/disco/pool/stable/amd64/docker-ce_19.03.3~3-0~ubuntu-disco_amd64.deb"
@@ -15,10 +15,10 @@ sudo dpkg -i "containerd.io_1.2.6-3_amd64.deb"
 sudo dpkg -i "docker-ce-cli_19.03.3~3-0~ubuntu-disco_amd64.deb"
 sudo dpkg -i "docker-ce_19.03.3~3-0~ubuntu-disco_amd64.deb"
 
-echo imagens disponiveis
+echo images available
 docker image ls
 
-echo imagens que consigo rodar
+echo images I can rotate
 docker ps --all
 
 
